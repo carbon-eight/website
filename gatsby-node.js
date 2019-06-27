2c70c4f429dac4ed13d641b0cbb387d7299ab25d
Allan Pooley <allanpooley@outlook.com>
2019-06-27 16:49:37 +1000

    ExpressionOfInterestTemplate stub and graphql

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,46 @@
-// const _ = require('lodash');
-// const website = require('./config/website');
-//
-// // graphql function doesn't throw an error so we have to check to check for the result.errors to throw manually
-// const wrapper = promise => promise.then((result) => {
-//   if (result.errors) throw result.errors;
-//   return result;
-// });
+
+// graphql function doesn't throw an error so we have to check to check for the result.errors to throw manually
+const wrapper = promise => promise.then((result) => {
+  if (result.errors) throw result.errors;
+  return result;
+});
 
 
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions;
-// };
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const interestPageTemplate = require.resolve('./src/templates/interest.jsx');
+
+  const interestPages = await wrapper(
+    graphql(`
+      {
+        allPrismicExpressionOfInterest {
+          edges {
+            node {
+              id
+              uid
+            }
+          }
+        }
+      }
+    `),
+  );
+
+  const interestPageList = interestPages.data.allPrismicExpressionOfInterest.edges;
+
+  /* ---------------------------------------------
+  = Create an individual page for each Story =
+  ----------------------------------------------- */
+
+  interestPageList.forEach((edge) => {
+    // The uid you assigned in Prismic is the slug!
+    createPage({
+      path: `/${edge.node.uid}/`,
+      component: interestPageTemplate,
+      context: {
+        // Pass the unique ID (uid) through context so the template can filter by it
+        uid: edge.node.uid,
+      },
+    });
+  });
+};
---
+++ src/templates/interest.jsx
@@ -0,0 +1,164 @@
+import React, { Component } from 'react';
+import { graphql } from 'gatsby';
+import { Layout, SliceZone } from '../components';
+
+class ExpressionOfInterestTemplate extends Component {
+  render() {
+    const {
+      data: {
+        page: {
+          data: pageData,
+        },
+      },
+      location,
+    } = this.props;
+    console.log('Props @ ExpressionOfInterestTemplate', this.props);
+    console.log({pageData});
+    const {
+      body,
+      heroTitle,
+      heroSubtitle,
+      metaTitle,
+      metaDescription,
+      openGraphImage,
+    } = pageData;
+    const seoData = {
+      metaTitle,
+      metaDescription,
+      openGraphImage,
+    };
+    console.log({ heroTitle });
+    return (
+      <Layout location={location} seoData={seoData}>
+        <h1 className="block-title">{heroTitle.text}</h1>
+        <SliceZone
+          allSlices={body}
+        />
+      </Layout>
+    );
+  }
+}
+
+export default ExpressionOfInterestTemplate;
+
+export const pageQuery = graphql`
+  query InterestPageBySlug($uid: String!) {
+    # site {
+    #   siteMetadata {
+    #     blogSlug,
+    #   }
+    # },
+    page: prismicExpressionOfInterest(uid: { eq: $uid }) {
+      data {
+        body {
+          ... on PrismicExpressionOfInterestBodyNewsletterSubscriber {
+            id
+            sliceType: slice_type
+            primary {
+              title {
+                html
+                text
+              }
+            }
+          }
+          ... on PrismicExpressionOfInterestBodyExplanationBlocks {
+            id
+            sliceType: slice_type
+            primary {
+              title {
+                text
+              }
+            }
+            items {
+              blockImage: block_image {
+                alt
+                localFile {
+                  childImageSharp {
+                    fluid(maxWidth: 800, quality: 90) {
+                      ...GatsbyImageSharpFluid_withWebp
+                    }
+                  }
+                }
+              }
+              description {
+                text
+              }
+              title {
+                text
+              }
+            }
+          }
+          ... on PrismicExpressionOfInterestBodyPartnerShowcase {
+            id
+            sliceType: slice_type
+            primary {
+              subtitle {
+                html
+                text
+              }
+              title {
+                html
+                text
+              }
+            }
+            items {
+              link {
+                url
+                target
+                link_type
+              }
+              name {
+                text
+              }
+              photo {
+                localFile {
+                  childImageSharp {
+                    fluid(maxWidth: 800, quality: 90) {
+                      ...GatsbyImageSharpFluid_withWebp
+                    }
+                  }
+                }
+                alt
+              }
+            }
+          }
+          ... on PrismicExpressionOfInterestBodyEnquiryForm {
+            id
+            sliceType: slice_type
+            primary {
+              formTitle: form_title {
+                html
+                text
+              }
+              formSubtitle: form_subtitle {
+                html
+                text
+              }
+            }
+          }
+        }
+        heroTitle: hero_title {
+          html
+          text
+        }
+        heroSubtitle: hero_subtitle {
+          html
+          text
+        }
+        metaTitle: meta_title {
+          html
+          text
+        },
+        metaDescription: meta_description {
+          html
+          text
+        },
+        openGraphImage: open_graph_image {
+          alt
+          copyright
+          url
+        }
+      }
+    }
+  }
+`;
