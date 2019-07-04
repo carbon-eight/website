20e3983253f48cc8dd65db55bfe67ebfe8322823
Allan Pooley <allanpooley@outlook.com>
2019-07-04 18:29:39 +1000

    Rellax parallax implementation

--- src/components/Mural/HomeMural.jsx
+++ src/components/Mural/HomeMural.jsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Rellax from 'rellax';
 import {
   CropScene,
   FarmScene,
   SoilScene,
   RootsScene,
   ValleyScene,
+  ParallaxElement,
 } from '.';
 import './HomeMural.scss';
 
-const HomeMural = (props) => {
-  const {
-    someProp,
-  } = props;
-  return (
-    <div className="home-mural">
-      <ValleyScene />
-      <FarmScene />
-      <CropScene />
-      <SoilScene />
-      <RootsScene />
-    </div>
-  );
-};
+class HomeMural extends Component {
+  componentDidMount() {
+    this.rellax = new Rellax('.parallax', {
+      center: false,
+      round: true,
+      vertical: true,
+      horizontal: false,
+    });
+  }
+
+  componentWillUnmount() {
+    this.rellax.destroy();
+  }
+
+  render() {
+    return (
+      <div className="home-mural">
+        <ParallaxElement parallaxSpeed={-7}>
+          <ValleyScene />
+        </ParallaxElement>
+        <ParallaxElement parallaxSpeed={-5}>
+          <FarmScene />
+        </ParallaxElement>
+        <ParallaxElement parallaxSpeed={-3}>
+          <CropScene />
+        </ParallaxElement>
+        <ParallaxElement parallaxSpeed={-1}>
+          <SoilScene />
+          <RootsScene />
+        </ParallaxElement>
+      </div>
+    );
+  }
+}
 
 export default HomeMural;
