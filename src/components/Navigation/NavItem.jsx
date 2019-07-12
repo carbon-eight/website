6c8f0b0342b3a69fd8b622e26a53f9410caa312e
Allan Pooley <allanpooley@outlook.com>
2019-07-12 10:58:30 +1000

    Updated NavLinks

--- src/components/Navigation/NavItem.jsx
+++ src/components/Navigation/NavItem.jsx
@@ -30,6 +30,11 @@ const NavItem = (props) => {
         aria-label={`Navigate to ${linkLabel.text} page`}
       >
         <span>{linkLabel.text}</span>
+        <div className="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 15">
+            <path fill="#26C281" fillRule="nonzero" d="M.818 12.5l2.121 2.121L10 7.561 2.94.5.817 2.621l4.94 4.94z" />
+          </svg>
+        </div>
       </Link>
       {includeDescription && (
         <p className="description">
