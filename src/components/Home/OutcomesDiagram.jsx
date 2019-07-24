99d49685d8b10f99f3ff7545b49f20c85cdd0f85
Allan Pooley <allanpooley@outlook.com>
2019-07-24 15:43:11 +1000

    Finally! Fixed!

--- src/components/Home/OutcomesDiagram.jsx
+++ src/components/Home/OutcomesDiagram.jsx
@@ -42,8 +42,8 @@ const OutcomesDiagram = (props) => {
   return (
     <div className="outcomes-diagram">
       <div className="container" ref={ref}>
-        { isMobile ? (
-          <div className="diagram diagram-mobile">
+        <Wrapper>
+          <div className="diagram">
             <div className={`outcome-illustration${loadAnimations ? ' visible' : ''}`}>
               <OutcomeIllustration
                 illustration={activeOutcome.illustration}
@@ -51,18 +51,14 @@ const OutcomesDiagram = (props) => {
                 loadAnimations={loadAnimations}
               />
             </div>
-            <div className="half-circle">
-              <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 360 99">
-                <defs>
-                  <path id="half-circle-a" d="M.924.5h360v100h-360z" />
-                </defs>
-                <g fill="none" fillRule="evenodd" transform="translate(-.924 -1.5)">
-                  <mask id="half-circle-b" fill="#fff">
-                    <use xlinkHref="#half-circle-a" />
-                  </mask>
-                  <path fill="#E8F4EF" fillRule="nonzero" d="M-40-121c0 122.055 98.945 221 221 221S402 1.055 402-121h-3C399-.602 301.398 97 181 97S-37-.602-37-121h-3z" mask="url(#half-circle-b)" />
-                </g>
-              </svg>
+            <div className="circle desktop-only">
+              <OutcomeButtons
+                outcomes={outcomes}
+                activeIndex={activeIndex}
+                setActiveIndexHandler={setActiveIndex}
+              />
+            </div>
+            <div className="half-circle mobile-only">
               <OutcomeButtons
                 outcomes={outcomes}
                 activeIndex={activeIndex}
@@ -80,36 +76,7 @@ const OutcomesDiagram = (props) => {
               />
             </div>
           </div>
-        ) : (
-          <Wrapper>
-            <div className="diagram diagram-desktop">
-              <div className="circle">
-                <OutcomeButtons
-                  outcomes={outcomes}
-                  activeIndex={activeIndex}
-                  setActiveIndexHandler={setActiveIndex}
-                />
-                <div className={`outcome-illustration${loadAnimations ? ' visible' : ''}`}>
-                  <OutcomeIllustration
-                    illustration={activeOutcome.illustration}
-                    isVisible={inView}
-                    loadAnimations={loadAnimations}
-                  />
-                </div>
-                <div className="outcome-elaboration">
-                  <h3 className="title">
-                    <span className={inView ? 'visible' : ''}>
-                      {activeOutcome.title.text}
-                    </span>
-                  </h3>
-                  <HtmlContent
-                    content={activeOutcome.description.html}
-                  />
-                </div>
-              </div>
-            </div>
-          </Wrapper>
-        )}
+        </Wrapper>
       </div>
     </div>
   );
