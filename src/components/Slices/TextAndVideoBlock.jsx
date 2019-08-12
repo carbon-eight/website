b1c1b3b3ec320f83a639ebccb744b094f0615266
Allan Pooley <allanpooley@outlook.com>
2019-08-12 18:43:16 +1000

    Progress on TextAndVideoBlock

--- src/components/Slices/TextAndVideoBlock.jsx
+++ src/components/Slices/TextAndVideoBlock.jsx
@@ -1,22 +1,54 @@
 import React from 'react';
-import { Wrapper } from '../common';
+import {
+  Wrapper,
+  HtmlContent,
+  VideoBlock,
+  CurlyDivider,
+} from '../common';
+import './TextAndVideoBlock.scss';
+import { COLOUR_TERTIARY, COLOUR_WHITE } from '../../util/colours';
 
 const TextAndVideoBlock = (props) => {
   const {
     data,
   } = props;
-  // const {
-  //   primary,
-  // } = data;
-  // const {
-  //   title,
-  // } = primary;
-  console.log({ data });
+  const {
+    primary,
+  } = data;
+  const {
+    title,
+    headline,
+    description,
+    videoProvider,
+    videoUrl,
+  } = primary;
   return (
     <section className="text-and-video-block">
+      <CurlyDivider
+        topColor={COLOUR_WHITE}
+        bottomColor={COLOUR_TERTIARY}
+      />
       <Wrapper>
-        <span>TextAndVideoBlock</span>
+        <div className="container">
+          <div className="col video-col">
+            <VideoBlock
+              videoProvider={videoProvider.toLowerCase()}
+              videoUrl={videoUrl.url}
+            />
+          </div>
+          <div className="col text-col">
+            <div className="text-container">
+              <span className="label">{headline.text}</span>
+              <h2 className="title">{title.text}</h2>
+              <HtmlContent className="description" content={description.html} />
+            </div>
+          </div>
+        </div>
       </Wrapper>
+      <CurlyDivider
+        topColor={COLOUR_TERTIARY}
+        bottomColor={COLOUR_WHITE}
+      />
     </section>
   );
 };
