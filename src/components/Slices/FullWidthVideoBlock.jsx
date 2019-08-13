607132a284a57171798e2963309dd36dc74e7f13
Allan Pooley <allanpooley@outlook.com>
2019-08-14 09:01:59 +1000

    Polishing about page

--- src/components/Slices/FullWidthVideoBlock.jsx
+++ src/components/Slices/FullWidthVideoBlock.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Wrapper, VideoBlock } from '../common';
+import {
+  Wrapper,
+  VideoBlock,
+  CurlyDivider,
+} from '../common';
+import { COLOUR_TERTIARY, COLOUR_WHITE } from '../../util/colours';
 import './FullWidthVideoBlock.scss';
 
 const FullWidthVideoBlock = (props) => {
@@ -15,15 +20,25 @@ const FullWidthVideoBlock = (props) => {
     videoUrl,
   } = primary;
   return (
-    <section className="full-width-video-block">
-      <Wrapper>
-        <h2 className="title">{title.text}</h2>
-        <VideoBlock
-          videoProvider={videoProvider.toLowerCase()}
-          videoUrl={videoUrl.url}
-        />
-      </Wrapper>
-    </section>
+    <>
+      <CurlyDivider
+        topColor={COLOUR_WHITE}
+        bottomColor={COLOUR_TERTIARY}
+      />
+      <section className="full-width-video-block">
+        <Wrapper>
+          <h2 className="title">{title.text}</h2>
+          <VideoBlock
+            videoProvider={videoProvider.toLowerCase()}
+            videoUrl={videoUrl.url}
+          />
+        </Wrapper>
+      </section>
+      <CurlyDivider
+        topColor={COLOUR_TERTIARY}
+        bottomColor={COLOUR_WHITE}
+      />
+    </>
   );
 };
 
