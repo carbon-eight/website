92c3ee882c9409bd2f232558ae211f460fae7da6
Allan Pooley <allanpooley@outlook.com>
2019-08-13 16:20:56 +1000

    NewsletterBlock polish

--- src/components/Slices/NewsletterBlock.jsx
+++ src/components/Slices/NewsletterBlock.jsx
@@ -5,8 +5,10 @@ import {
   Image,
 } from '../common';
 import {
+  ColourfulBackground,
   SubscribeForm,
 } from '..';
+import { SQUARE_BACKGROUND_TYPE } from '../ColourfulBackground';
 import './NewsletterBlock.scss';
 
 const NewsletterBlock = (props) => {
@@ -35,7 +37,16 @@ const NewsletterBlock = (props) => {
             </div>
           </div>
           <div className="col image-col">
-            <Image image={photo} />
+            <div className="image-container">
+              <div className="colourful-background">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 536 536">
+                  <path fill="#E8F4EF" fillRule="evenodd" d="M268.231 535.277c73.868 0 123.647-47.889 172.055-96.297 48.408-48.408 95.446-97.335 95.446-171.203 0-73.868.872-150.94-47.536-199.348C463.556 43.79 304.5.277 268.23.277c-73.868 0-113.506 74.518-161.914 122.926C57.909 171.61.731 193.909.731 267.777c0 73.868-9.377 134.092 39.031 182.5 48.408 48.408 154.601 85 228.47 85z" />
+                </svg>
+              </div>
+              <div className="inner-image">
+                <Image image={photo} />
+              </div>
+            </div>
           </div>
         </div>
       </Wrapper>
